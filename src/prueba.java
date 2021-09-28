33acf93d68b9de25aea9e5b95badbfbb2c56785b
José Luis <jlrod2@gmail.com>
2021-09-28 21:09:13 +0200

    Cambiado mensaje de la app

--- src/prueba.java
+++ src/prueba.java
@@ -1,5 +1,5 @@
 public class prueba {
     public static void main (String [ ] args) {
-        System.out.println("Probando el entorno");
+        System.out.println("usando git desde el IDE");
     }
 }
