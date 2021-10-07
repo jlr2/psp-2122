5d76c5c19b8aeec0383b43679f44a985110affca
José Luis <jlrod2@gmail.com>
2021-10-07 09:05:04 +0200

    Cambiado mensaje de la app

--- prueba2.java
+++ prueba2.java
@@ -1,5 +1,6 @@
 public class prueba {
     public static void main (String [ ] args) {
-        System.out.println("usando git desde el IDE22");
+        System.out.println("usando git desde el IDE");
     }
 }
+
