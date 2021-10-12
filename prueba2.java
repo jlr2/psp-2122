4e48f098317f698bc40e0ea68d29987525018082
José Luis <jlrod2@gmail.com>
2021-10-12 18:31:08 +0200

    Modificado de nuevo fichero prueba2.java

--- prueba2.java
+++ prueba2.java
@@ -1,6 +1,6 @@
 public class prueba {
     public static void main (String [ ] args) {
-        System.out.println("usando git desde Github");
+        System.out.println("usando git desde Github y sincronizado desde el IDE");
     }
 }
 
